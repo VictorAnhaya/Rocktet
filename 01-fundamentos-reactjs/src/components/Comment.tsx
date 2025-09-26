ddcdb9b1268db1951af93717d4a278d4586203fa
VictorAnhaya <162132980+VictorAnhaya@users.noreply.github.com>
2025-09-26 17:01:05 -0300

    50

--- 01-fundamentos-reactjs/src/components/Comment.tsx
+++ 01-fundamentos-reactjs/src/components/Comment.tsx
@@ -2,24 +2,36 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment(props){
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/VictorAnhaya.png" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
-                <header>
-                    <div className={styles.authorAndTime}>
-                        <strong>Victor Anhaya</strong>
-                        <time title="02 de Setembro às  14:33"   dateTime="2025-08-31 15:33:03">Cerca de 1h atrás</time>
-                    </div>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Victor Anhaya</strong>
+                            <time title="02 de Setembro às 14:33" dateTime="2025-08-31 15:33:03">
+                                Cerca de 1h atrás
+                            </time>
+                        </div>
 
-                    <button title="Deletar Comentário">
-                        <Trash size={24} />
-                    </button>
-                </header>
-                <p>{props.content}</p>
+                        <button onClick={handleDeleteComment} title="Deletar Comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
--- 01-fundamentos-reactjs/src/components/Post.tsx
+++ 01-fundamentos-reactjs/src/components/Post.tsx
@@ -33,10 +33,27 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText('')
     }
 
-
+   
     function handleNewCommentChange() {
+         event.target.setCustomValidity('');
         setNewCommentText(event.target.value);             
     }
+
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório');
+    }
+
+    function deleteComment(commentToDelete){
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+        return comment !==  commentToDelete;
+        })
+
+        setComments(commentsWithoutDeletedOne);
+
+    }
+
+    const  isNewCommentEmpty = newCommentText.length == 0;
 
     return(
      <article className={styles.post}>
@@ -71,16 +88,25 @@ export function Post({ author, publishedAt, content }) {
             placeholder="Deixe seu comentário"
             value={newCommentText}
             onChange={handleNewCommentChange}
+            onInvalid={handleNewCommentInvalid}
+            required
             />  
 
             <footer>
-            <button type="submit">Publicar</button>     
+            <button type="submit" disabled={isNewCommentEmpty}>
+                Publicar</button>     
             </footer>     
         </form>
 
         <div className={styles.commentList}>
         {comments.map(comment => {
-            return <Comment key={comment} content={comment} />
+            return (
+            <Comment
+             key={comment} 
+             content={comment} 
+             onDeleteComment={deleteComment} 
+              />
+            )
         })}
         </div>
      </article>
